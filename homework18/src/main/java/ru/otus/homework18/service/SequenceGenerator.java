ac38873a2b546019fde09966ded133f048216383
siatl <siatl2@mail.ru>
2022-07-25 21:26:04 +0300

    edit by comments

--- homework18/src/main/java/ru/otus/homework18/service/SequenceGenerator.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.homework18.service;
-
-import reactor.core.publisher.Mono;
-import ru.otus.homework18.model.Counter;
-
-public interface SequenceGenerator {
-    public Mono<Counter> getNextCounter(String sequenceName);
-}
